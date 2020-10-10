7859a0f5a3c86198d47b9bcf0cb7897dffb5c476
Sewwandi Nisansala <sewwanis@gmail.com>
2020-10-10 23:36:15 +0530

    updated doctor

--- src/main/java/lk/sparkx/ncms/controller/DoctorServlet.java
+++ src/main/java/lk/sparkx/ncms/controller/DoctorServlet.java
@@ -1,6 +1,7 @@
 package lk.sparkx.ncms.controller;
 
 import lk.sparkx.ncms.dao.Doctor;
+import lk.sparkx.ncms.db.DBConnectionPool;
 import lk.sparkx.ncms.repository.DoctorRepository;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 
 @WebServlet(name = "DoctorServlet")
@@ -58,10 +61,52 @@ public class DoctorServlet extends HttpServlet {
         Doctor doctor = new Doctor();
         doctor.loadData(doctor);
         System.out.println("successfully loaded");
-        String doctorId = req.getParameter("doctorId");
 
     }
 
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        String name = req.getParameter("name");
+        String hospitalId = req.getParameter("hospital_id");
+        Boolean isDirector = Boolean.valueOf(req.getParameter("is_director"));
 
+        try {
+            Connection con = DBConnectionPool.getInstance().getConnection();
+
+            PreparedStatement pstmt = con.prepareStatement("UPDATE doctor SET  id=?,name=?, hospital_id=?, is_director=? WHERE id=?");
+
+            pstmt.setInt(1,id);
+            pstmt.setString(2,name);
+            pstmt.setString(3, hospitalId);
+            pstmt.setBoolean(4, isDirector);
+
+            con.close();
+
+            int result = pstmt.executeUpdate();
+            if (result != 0){
+                System.out.println("Successfully updated");
+            }else{
+                System.out.println("Update failed");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try{
+            Connection con = DBConnectionPool.getInstance().getConnection();
+
+            int doctor_id = Integer.parseInt(req.getParameter("id"));
+
+            PreparedStatement pstmt = con.prepareStatement("DELETE FROM doctor WHERE id=?");
+            pstmt.setInt(1, doctor_id);
+            pstmt.executeUpdate();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
